0ca8c1c5738d050651b991467a18796d984885f6
Satyam <7satyamjha@gmail.com>
2021-11-05 22:49:20 +0530

    location as object

--- Server/SendLoc.js
+++ Server/SendLoc.js
@@ -1,28 +1,28 @@
-import React,{useContext} from "react";
+import React, { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import firebase from "firebase";
 import { AuthenticationContext } from "../authentication/authentication.context";
 
 export default function SendLoc(userLoc) {
-  const { user} = useContext(AuthenticationContext);
+  const { user } = useContext(AuthenticationContext);
   console.log("Sendloc.js")
   // console.log(,"0-------------------------------------")
-  console.log(user,"dfkdjfdjfdkjflkdjfldjfljfldkjfdkjfl")
-  
-  let url = `https://bustrack-27015-default-rtdb.asia-southeast1.firebasedatabase.app/BusLocation/${user["user"]["email"].slice(0,-4)}.json`;
-  console.log(url);
+
+
+  let url = `https://bustrack-27015-default-rtdb.asia-southeast1.firebasedatabase.app/BusLocation/${user["user"]["email"].slice(0, -4)}.json`;
+
 
   fetch(
     url,
     {
-      method: "PUT", 
+      method: "PUT",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(userLoc),
+      body: JSON.stringify({ "latitude": userLoc["userLoc"]["latitude"], "longitude": userLoc["userLoc"]["longitude"] }),
     }
   );
 
   return <View>
-    {console.log("send",userLoc)}</View>;
+    {console.log("send", userLoc)}</View>;
 }
